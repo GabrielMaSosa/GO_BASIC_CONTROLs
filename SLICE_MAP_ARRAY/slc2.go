aeb42dc933ce7a25bcd6a4fd23f738dd79fd6f1c
GabrielMaSosa <142988457+GabrielMaSosa@users.noreply.github.com>
2023-08-30 17:12:55 -0300

    se hace commit con cpacidad en slc2.go

--- SLICE_MAP_ARRAY/slc2.go
+++ SLICE_MAP_ARRAY/slc2.go
@@ -7,9 +7,12 @@ import (
 func main() {
 	//a := make([]string, 6) creamos un slice con valores nulos
 	var a []string
-	a = append(a, "Lunes", "Martes", "Miercoles", "Jueves")
-	fmt.Println(a[0:3])
+	a = append(a, "Lunes", "Martes", "Miercoles", "Jueves") //agregar al slice
+	fmt.Println(a[0:3])                                     //imprimo con un rango
 	fmt.Println(a)
+
+	//si agrego otro elemento la capacidad se me duplica y el len aumenta al valor actual
+	a = append(a, "Sabado")
 	fmt.Println(len(a))
 	fmt.Println(cap(a))
 }
